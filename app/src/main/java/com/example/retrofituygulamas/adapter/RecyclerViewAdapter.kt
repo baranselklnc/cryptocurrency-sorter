2988626d8826153368f07052c042804719afb2ce
Baransel <baranselklnc@gmail.com>
2023-08-31 18:07:27 +0300

    initialized

--- app/src/main/java/com/example/retrofituygulamas/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/retrofituygulamas/adapter/RecyclerViewAdapter.kt
@@ -10,18 +10,20 @@ import com.example.retrofituygulamas.model.CryptoModel
 
 class RecyclerViewAdapter(private val cryptoList:ArrayList<CryptoModel>): RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
     class RowHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind(){
+        fun bind(cryptoModel: CryptoModel){
 
             val textView: TextView = itemView.findViewById(R.id.text_name) // Buradaki R.id.textViewId, layout dosyanızdaki TextView'in ID'si olmalıdır
             val textView2: TextView = itemView.findViewById(R.id.text_price)
+            textView.text=cryptoModel.currency
+            textView2.text=cryptoModel.price
 
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
-  val view =LayoutInflater.from(parent.context).inflate(R.layout.raw_layout,parent,false)
-        return RowHolder(view)
+         val view =LayoutInflater.from(parent.context).inflate(R.layout.raw_layout,parent,false)
+         return RowHolder(view)
 
     }
 
