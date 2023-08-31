7000cd7337047cbef4b08dbde935e0214223e91e
Baransel <baranselklnc@gmail.com>
2023-08-31 18:38:26 +0300

    The connection between recycler view and adapter is realised . Otherwise colour palette was created.

--- app/src/main/java/com/example/retrofituygulamas/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/retrofituygulamas/adapter/RecyclerViewAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.retrofituygulamas.adapter
 
+import android.graphics.Color
+import android.location.GnssAntennaInfo.Listener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,15 +9,27 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofituygulamas.R
 import com.example.retrofituygulamas.model.CryptoModel
+import okhttp3.internal.http2.Http2Connection
 
-class RecyclerViewAdapter(private val cryptoList:ArrayList<CryptoModel>): RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
+class RecyclerViewAdapter(private val cryptoList:ArrayList<CryptoModel>,private val listener:Listener): RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
+ interface Listener {
+     fun OnItemClick(cryptoModel: CryptoModel)
+ }
+
+   private val colors:Array<String> = arrayOf("#7a53f9","#c28bbb","#21b2b6","#f7aa1f","#f953d2","#f71f6c","#d2f953","#5866ff","#5b235c")
+
+
     class RowHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind(cryptoModel: CryptoModel){
+        fun bind(cryptoModel: CryptoModel,colors:Array<String>,position: Int,listener:Listener){
+            itemView.setOnClickListener {
+                listener.OnItemClick(cryptoModel)
+            }
 
             val textView: TextView = itemView.findViewById(R.id.text_name) // Buradaki R.id.textViewId, layout dosyanızdaki TextView'in ID'si olmalıdır
             val textView2: TextView = itemView.findViewById(R.id.text_price)
             textView.text=cryptoModel.currency
             textView2.text=cryptoModel.price
+            itemView.setBackgroundColor(Color.parseColor(colors[position%9]) )
 
         }
 
@@ -32,6 +46,7 @@ class RecyclerViewAdapter(private val cryptoList:ArrayList<CryptoModel>): Recycl
     }
 
     override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        holder.bind(cryptoList[position],colors,position,listener)
 
     }
 }
